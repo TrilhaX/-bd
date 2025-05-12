801a38d6c39a78bf183e3ab4688a1ae073a7f7dd
TrilhaX <joaotrilha01@gmail.com>
2025-05-12 11:27:25 -0300

    Arrumado o erro que estava tendo

--- db.js
+++ db.js
@@ -2,15 +2,19 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 const dbConnection = require('./index')
-const db = dbConnection()
+let db
+dbConnection().then(retorno => {
+    db = retorno
+})
+
 
 app.post('/usuarios', async (req, res) => {
     const {nome, email} = req.body
     try{
-        const result = await db.run(`INSERT INTO usuarios (nome, email) VALUES ('Ramon', 'ramon.brignoli@edu.sc.senai.br')`)
+        const result = await db.run(`INSERT INTO usuarios (nome, email) VALUES (?, ?)`, [nome, email])
         res.status(201).json(({msg: "Criado com sucesso"}))
     }catch(err){
-        res.status(500).json({msg: `${err.msg}`})
+        res.status(500).json({msg: `${err.message}`})
     }
 })
 
